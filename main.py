4b464f29b98512291717e8ddb0ae108cea48b30c
lukas.hofer <lukas.hofer@students.fhnw.ch>
2022-12-29 21:47:29 +0100

    additions

--- main.py
+++ main.py
@@ -10,26 +10,28 @@ import threading
 import time
 import requests
 import json
+import pandas as pd
 
+results = pd.DataFrame()
+print("hier")
 
 class IBapi(EWrapper, EClient):
     def __init__(self):
         EClient.__init__(self, self)
 
     def historicalData(self, reqId, bar):
+        time.sleep(2)
         print("HistoricalData. ReqId:", reqId, "BarData. ", bar)
 
     def tickPrice(self, reqId, tickType, price, attrib):
         print('The current ask price is: ', price)
 
-    def securityDefinitionOptionParameter(self, reqId: int, exchange: str, underlyingConId: int, tradingClass: str,
-                                          multiplier: str, expirations: SetOfString, strikes: SetOfFloat):
+    def securityDefinitionOptionParameter(self, reqId: int, exchange: str, underlyingConId: int, tradingClass: str, multiplier: str, expirations: SetOfString, strikes: SetOfFloat):
         print("here")
         for expiration in expirations:
             for strike in strikes:
-                time.sleep(1)
                 temp = Contract()
-                temp.symbol = {tradingClass}
+                temp.symbol = tradingClass
                 temp.secType = "OPT"
                 temp.exchange = "SMART"
                 temp.currency = "USD"
@@ -37,11 +39,9 @@ class IBapi(EWrapper, EClient):
                 temp.strike = strike
                 temp.right = "C"
                 temp.multiplier = "100"
-                time.sleep(1)
-                tmper = app.reqHistoricalData(0, contract, "", "1 D", "1 min", "TRADES", 0, 1, False, [])
-                time.sleep(1)
-                print(tmper)
                 print(f"Expiration: {expiration}, Strike: {strike}, Exchange: {exchange}, Company: {tradingClass}")
+                time.sleep(0.2)
+                app.reqHistoricalData(5, temp, queryTime, "2 D", "4 hours", "TRADES", 1, 1, False, [])
 
     def accountSummary(self, reqId: int, account: str, tag: str, value: str, currency: str):
         super().accountSummary(reqId, account, tag, value, currency)
@@ -55,9 +55,11 @@ def run_loop():
 
 app = IBapi()
 app.connect('127.0.0.1', 7496, 123)
-api_thread = threading.Thread(target=run_loop, daemon=True)
+api_thread = threading.Thread(target=app.run, daemon=True)
 api_thread.start()
-time.sleep(1)
+time.sleep(10)
+
+print("haha")
 
 contract = Contract()
 contract.symbol = "MSFT"
@@ -69,16 +71,13 @@ contract.strike = 235
 contract.right = "C"
 contract.multiplier = "100"
 
-# app.reqMktData(1, contract, '', False, False, [])
-
-
 queryTime = (datetime.datetime.today() - datetime.timedelta(days=1)).strftime("%Y%m%d-%H:%M:%S")
-tmper = app.reqHistoricalData(5, contract, queryTime, "2 D", "4 hours", "TRADES", 1, 1, False, [])
-# app.reqSecDefOptParams(0, "IBM", "", "STK", 8314)
+app.reqHistoricalData(5, contract, queryTime, "2 D", "4 hours", "TRADES", 1, 1, False, [])
+app.reqSecDefOptParams(0, "IBM", "", "STK", 8314)
+print("hereee")
+time.sleep(100)
+print("now proceeding")
 
-time.sleep(5)
-app.disconnect()
-print("disconnected, now getting new data")
 
 # Create a list of the top 100 stocks in the US
 top_100_stocks = ['AAPL', 'GOOG', 'MSFT', 'AMZN', 'FB', 'JPM', 'XOM', 'V', 'BAC', 'WFC',
@@ -93,7 +92,6 @@ top_100_stocks = ['AAPL', 'GOOG', 'MSFT', 'AMZN', 'FB', 'JPM', 'XOM', 'V', 'BAC'
                   'MDT', 'APC', 'CNI', 'LUV', 'GIS', 'VIAB', 'HP', 'XRX', 'CL']
 stock_data = {}
 x = 1
-
 for stock in top_100_stocks:
     stock_info = yf.Ticker(stock).info
     closing_price = stock_info['regularMarketPrice']
@@ -104,3 +102,9 @@ for stock in top_100_stocks:
 print(stock_data)
 print("end)")
 
+
+
+
+app.disconnect()
+
+#app.reqMktData(1, contract, '', False, False, [])
